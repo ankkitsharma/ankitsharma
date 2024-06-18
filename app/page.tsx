121dd994f267e10e2320fe162875c152ad6cdee2
ankkitsharma <ankitsharma2801@gmail.com>
2024-06-18 22:24:59 +0530

    fix theme switching

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ToggleBtn from "@/components/ToggleBtn";
 export default function page() {
   return (
-    <>
+    <div>
       <ToggleBtn />
-      <div>page</div>
-    </>
+      <div>Page</div>
+    </div>
   );
 }
--- components/ToggleBtn.tsx
+++ components/ToggleBtn.tsx
@@ -4,17 +4,21 @@ import styles from "./ToggleBtn.module.css";
 import useLocalStorage from "use-local-storage";
 import { Toggle } from "@/components/Toggle";
 
-export default function ToggleBtn() {
+export default function ToggleBtn(props: any) {
   const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const [isDark, setIsDark] = useLocalStorage("isDark", preference);
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  }, [isDark]);
 
   return (
     <div className={styles.App} data-theme={isDark ? "dark" : "light"}>
       <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
-      <h1 className={styles.title}>Hello world!</h1>
-      <div className={styles.box}>
-        <h2>This is a box</h2>
-      </div>
     </div>
   );
 }
