4f07fafc1a54e64bc8a9bc6a7730b6973e00e382
ankkitsharma <ankitsharma2801@gmail.com>
2024-06-24 21:05:17 +0530

    added loading screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
 import styles from "./index.module.css";
 import LeftPart from "@/components/LeftPart";
 import RightPart from "@/components/RightPart";
+import { Suspense } from "react";
+import Loader from "@/components/Loading";
 
 export default function page() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000); // Adjust the delay time as needed (2000 milliseconds = 2 seconds)
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <div className={styles.home}>
-      <LeftPart />
-      <RightPart />
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className={styles.home}>
+          <LeftPart />
+          <RightPart />
+        </div>
+      )}
+    </>
   );
 }
---
+++ components/Loading.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import styles from "./Loading.module.css";
+
+export default function loading() {
+  return (
+    <div className={styles.loading}>
+      <div className={styles.loadingText}>Ankit Sharma Portfolio</div>
+    </div>
+  );
+}
